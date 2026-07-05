096ec735ad055e5520e2a3dd06d4ee2b419fd07e
Sidhartha Sankar Panda <sidharthasankarpanda6@gmail.com>
2026-07-05 23:28:11 +0530

    Update times.py

--- times.py
+++ times.py
@@ -9,7 +9,7 @@ model=pickle.load(pickle_in)
 
 @app.get('/')
 def index():
-    return {'Deployment': 'Hello and Welcome to 5 Minutes Engineering'}
+    return {'Deployment': 'Hello and Welcome'}
 
 @app.post('/predict')
 def predict(year:int ,month:int ,day:int):
